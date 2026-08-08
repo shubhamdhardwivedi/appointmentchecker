f2ebbda028149df2d6a486044891d7ec86c606b4
shubhamdhardwivedi <shubham@noetherdatalabs.ai>
2026-08-08 21:57:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ SELECT_LOCATION_TEXT = "Ausländeramt Aachen - Aachen Arkaden, Trierer Straße 1
 # between today and 20th August 2026:
 #   TARGET_WINDOW_START = date(2026, 8, 8)
 #   TARGET_WINDOW_END   = date(2026, 8, 20)
-TARGET_WINDOW_START = date(2026, 9, 7)     # yyyy,mm,dd <-- EDIT ME
+TARGET_WINDOW_START = date(2026, 8, 8)     # yyyy,mm,dd <-- EDIT ME
 TARGET_WINDOW_END = date(2026, 9, 9)     # yyyy,mm,dd <-- EDIT ME
 
 # If the automatic time-slot detection below ever seems wrong (reports a time
